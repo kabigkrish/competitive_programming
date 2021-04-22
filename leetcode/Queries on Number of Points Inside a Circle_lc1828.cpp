576e58467f9fa56f6eed969382052cac1d011245
kabigkrish <kabileshgopalakrishnan@gmail.com>
2021-04-22 20:45:25 +0530

    O(m*n)

--- leetcode/Queries on Number of Points Inside a Circle_lc1828.cpp
+++ leetcode/Queries on Number of Points Inside a Circle_lc1828.cpp
@@ -10,30 +10,20 @@
 using namespace std;
 class Solution {
 public:
-    void countPoints(vector<vector<int>>& points, vector<vector<int>>& queries)  
+    vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries)  
 {
 vector<int>answer;
 for(int j=0;j<queries.size();j++)
 {
-int range_left=queries[j][0]-queries[j][2];
-int range_right=queries[j][0]+queries[j][2];
-int range_top=queries[j][1]+queries[j][2];
-int range_bottom=queries[j][1]-queries[j][2];
 int counter=0;
-cout<<"------------"<<endl;
 for(int i=0;i<points.size();i++)
-{   
-    if(points[i][0]>=range_left &&points[i][0]<=range_right &&points[i][1]>=range_bottom&&points[i][1]>=range_bottom
-        &&sqrt(pow(queries[j][0]-points[i][0],2)+pow(queries[j][1]-points[i][1],2))<=queries[j][2] )
-    { 
-        counter++;
-        cout<<points[i][0]<<" "<<points[i][1]<<endl;
-    }
+{
+if(sqrt(pow(queries[j][0]-points[i][0],2)+pow(queries[j][1]-points[i][1],2))<=queries[j][2] )
+counter++;
 }
 answer.push_back(counter);
 }
-for(auto xd:answer)
-cout<<xd;
+return answer;
 }
 };
 
