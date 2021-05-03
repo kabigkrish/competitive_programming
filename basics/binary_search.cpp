595abb885659cd3c8a4ad4fb34a8462d03d515cb
kabigkrish <kabileshgopalakrishnan@gmail.com>
2021-05-03 19:02:16 +0530

    o(n/2)

---
+++ basics/binary_search.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[6]={10,20,30,40,50,60};
+    int x=40;
+    int low=0, high=6;
+    while(low<high)
+    {
+        int mid=(low+high)/2;
+        if(x==a[mid]) 
+        {
+            cout<<mid; 
+            break;
+        }
+        else if (x>a[mid]) low=mid+1;
+        else if (x<a[mid]) high =mid-1;
+    }
+} 
