86449a96a713cff9414b2d3e4d9dc89938240d83
kabigkrish <kabileshgopalakrishnan@gmail.com>
2021-05-04 11:59:37 +0530

    o(log N)

--- leetcode/Find First and Last Position of Element in Sorted Array_lc34.cpp
+++ leetcode/Find First and Last Position of Element in Sorted Array_lc34.cpp
@@ -26,24 +26,36 @@ public:
         else if(nums[mid]<target)
             return lastocc(nums,target,mid+1,high);
         else 
-            if(mid==0||nums[mid+1]!=nums[mid])
+            if(mid==nums.size()-1||nums[mid+1]!=nums[mid])
             return mid;
             else 
             return lastocc(nums,target,mid+1,high);
     }
     vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int>out={-1,-1};
+        if(nums.size()==0) return out;
         int low=0;
         int high=nums.size()-1;
         int first=firstocc(nums, target,low,high);
         int loww=0;
         int highh=nums.size()-1;
         int last=lastocc(nums,target,loww,highh);
-        vector<int>out;
-        out.push_back(first);
-        out.push_back(last);
+        out[0]=first;
+        out[1]=last;
         return out;   
     }
 };
+    // vector<int> searchRange(vector<int>& nums, int target) {
+        
+    //     int k=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+    //     int m=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+                    
+    //     if(k==m) return {-1,-1};
+        
+    //     return{k,m-1};
+        
+    // }
+
 int main()
 {
     Solution s;
