7df7b0b8e97fcd8f8bcdff948398783a8b16e6be
kabigkrish <kabileshgopalakrishnan@gmail.com>
2021-05-10 10:19:53 +0530

    O(n/2)

---
+++ leetcode/Container With Most Water_lc11.cpp
@@ -0,0 +1,65 @@
+// 11. Container With Most Water
+// Medium
+
+// Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+// Notice that you may not slant the container.
+
+ 
+
+// Example 1:
+
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+// Example 2:
+
+// Input: height = [1,1]
+// Output: 1
+
+// Example 3:
+
+// Input: height = [4,3,2,1,4]
+// Output: 16
+
+// Example 4:
+
+// Input: height = [1,2,1]
+// Output: 2
+
+ 
+
+// Constraints:
+
+//     n == height.length
+//     2 <= n <= 105
+//     0 <= height[i] <= 104
+
+#include<bits/stdc++.h>
+using namespace std;
+ class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int area=0;
+        int low=0;
+        int high=height.size()-1;
+        while(low<=high)
+        {
+            int dist=high-low;
+            int leng=min(height[low],height[high]);
+            area=max(leng*dist,area); 
+            if(height[low]<height[high]) low++;
+            else high--;
+        }
+        return area;
+    }
+    };
+int main()
+{
+    vector<int> height={1,8,6,2,5,4,8,3,7};
+    Solution solution;
+    int x=solution.maxArea(height);
+    cout<<x;
+
+}
