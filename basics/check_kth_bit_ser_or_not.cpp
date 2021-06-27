f62c3c4dd3663347460f1afc86380b2bc73e4179
kabigkrish <kabileshgopalakrishnan@gmail.com>
2021-06-27 22:30:51 +0530

    O(1)

--- basics/check_kth_bit_ser_or_not.cpp
+++ basics/check_kth_bit_ser_or_not.cpp
@@ -1,5 +1,5 @@
-//using right shift operator 
-
+// //using right shift operator 
+// The left-shift by 1 and right-shift by 1 are equivalent to the product of first term and 2 to the power given element(1<<3 = 1*pow(2,3)) and division of first term and second term raised to power 2 (1>>3 = 1/pow(2,3)) respectively. 
 #include<bits/stdc++.h>
 using namespace std;
 
