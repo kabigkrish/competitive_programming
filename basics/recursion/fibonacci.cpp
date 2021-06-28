d2ce0616e590e07cb3f1d9b2ac4a0761ef5a5768
kabigkrish <kabileshgopalakrishnan@gmail.com>
2021-06-28 18:05:23 +0530

    O(n)

---
+++ basics/recursion/fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int  fib(int n)
+{
+ /*base case n==0 return 0
+             n==1 return 1*/
+
+    if(n<=1) return n;
+    return fib(n-2)*fib(n-1);
+}
+int main()
+{
+    int n=5;
+    cout<<fib(5);
+}
