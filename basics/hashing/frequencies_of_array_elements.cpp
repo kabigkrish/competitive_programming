198db001d96d4b66cceda73675193742e202cde5
kabigkrish <kabileshgopalakrishnan@gmail.com>
2021-07-18 19:03:40 +0530

    .

--- basics/hashing/frequencies_of_array_elements.cpp
+++ basics/hashing/frequencies_of_array_elements.cpp
@@ -1,35 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void printfreq(int arr[],int n)
+void printfreq(int arr[], int n)
 {
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        bool flag=false;
-        for(int j=0;j<i;j++)
+        bool flag = false;
+        for (int j = 0; j < i; j++)
         {
-            if(arr[i]==arr[j]){ flag=true;break;}
+            if (arr[i] == arr[j])
+            {
+                flag = true;
+                break;
+            }
         }
-        if(flag==true)
-        continue;
-        int freq=1;
-        for(int j=i+1;j<n;j++)
-        if(arr[i]==arr[j])
-        freq++;
-        cout<<arr[i]<<" "<<freq<<endl;
-
+        if (flag == true)
+            continue;
+        int freq = 1;
+        for (int j = i + 1; j < n; j++)
+            if (arr[i] == arr[j])
+                freq++;
+        cout << arr[i] << " " << freq << endl;
     }
 }
-void efficient(int arr[],int n)
+void efficient(int arr[], int n)
 {
-    unordered_map<int,int> h;
-    for(int i=0;i<n;i++)
-    h[arr[i]]++;
-    for(auto i:h)
-    cout<<i.first<<" "<<i.second<<endl;
+    unordered_map<int, int> h;
+    for (int i = 0; i < n; i++)
+        h[arr[i]]++;
+    for (auto i : h)
+        cout << i.first << " " << i.second << endl;
 }
 int main()
 {
-    int arr[5]={1,2,3,1,2};
-    int n=5;
-    efficient(arr,n);
-} 
+    int arr[5] = {1, 2, 3, 1, 2};
+    int n = 5;
+    efficient(arr, n);
+}
---
+++ basics/strings/leftmost_non_repeating_element.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int naive(string &s)
+{
+    return 0;
+}
+
+int eff(string &s)
+{
+    return 0;
+}
+
+int main()
+{
+    string s = "aabbccdef";
+    cout << naive(s) << endl;
+    cout << eff(s) << endl;
+}
