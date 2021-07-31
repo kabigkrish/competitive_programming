6ed3483aa3e06f2fa810105fb20d726b9fc1684a
kabigkrish <kabileshgopalakrishnan@gmail.com>
2021-07-31 11:22:02 +0530

    .

--- leetcode/MaxIncreasetoKeepCitySkyline_lc_807.cpp
+++ leetcode/MaxIncreasetoKeepCitySkyline_lc_807.cpp
@@ -57,18 +57,37 @@ for(int i=0;i<grid.size();i++)
 */
 #include <bits/stdc++.h>
 using namespace std;
+class Solution
+{
+public:
+    int maxIncreaseKeepingSkyline(vector<vector<int>> &grid)
+    {
+        vector<int> row(grid[0].size(), 0);
+        vector<int> col(grid.size(), 0);
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                row[i] = max(row[i], grid[i][j]);
+                col[j] = max(col[j], grid[i][j]);
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid[0].size(); j++)
+            {
+                sum += abs(grid[i][j] - min(row[i], col[j]));
+            }
+        }
+        return sum;
+    }
+};
 
 int main()
 {
-    int arr[4][4] = {{8, 4, 8, 7},
-                     {7, 4, 7, 7},
-                     {9, 4, 8, 7},
-                     {3, 3, 3, 3}};
-    for (int i = 0; i < (sizeof(arr[i]) / sizeof(arr[i][0])); i++)
-    {
-        for (int j = 0; j < (sizeof(arr[i]) / sizeof(arr[i][0])); j++)
-        {
-            cout << arr[i][j];
-        }
-    }
+    Solution s;
+    vector<vector<int>> grid = {{1, 2, 3, 4, 5}, {2, 4, 5, 6, 7}, {8, 6, 4, 5, 6}, {9, 7, 5, 6, 7}, {6, 6, 7, 8, 9}};
+
+    int asf = s.maxIncreaseKeepingSkyline(grid);
 }
