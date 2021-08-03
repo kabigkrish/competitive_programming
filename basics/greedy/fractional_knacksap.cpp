ec353a229cbb6864d4d25053e030859d43b29f35
kabigkrish <kabileshgopalakrishnan@gmail.com>
2021-08-03 10:08:34 +0530

    .

---
+++ basics/greedy/fractional_knacksap.cpp
@@ -0,0 +1,67 @@
+/*
+(weight,value)={(50,600),(20,500),(30,400)}
+
+1.calculate ratio(value/weight) for every item
+2.sort every item in decreasing order of ratio
+3.initialize res=0,curr_cap=given_cap
+
+4.do following for every item i in sorted order 
+a)if(i.weight<=currcap) 
+    res+=i.value;
+    curr-cap=i.weight;
+ else 
+ {
+     res+=(i.value)*(curr_cap/i.weight);
+     ret res;
+
+ }
+ */
+#include <iostream>
+#include <cmath>
+#include <bits/stdc++.h>
+#include <climits>
+using namespace std;
+
+bool myCmp(pair<int, int> a, pair<int, int> b)
+{
+    double r1 = (double)a.first / a.second;
+
+    double r2 = (double)b.first / b.second;
+
+    return r1 > r2;
+}
+
+double fKnapS(int W, pair<int, int> arr[], int n)
+{
+    sort(arr, arr + n, myCmp);
+
+    double res = 0.0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i].second <= W)
+        {
+            res += arr[i].first;
+
+            W = W - arr[i].second;
+        }
+        else
+        {
+            res += arr[i].first * ((double)W / arr[i].second);
+
+            break;
+        }
+    }
+
+    return res;
+}
+int main()
+{
+    pair<int, int> arr[] = {make_pair(120, 30), make_pair(100, 20), make_pair(60, 10)};
+
+    int n = 3, W = 50;
+
+    cout << fKnapS(W, arr, n);
+
+    return 0;
+}
